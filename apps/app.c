31ebb4e198996badaca7f340a4e4fb8049aa4cea
IttaloSantos <ittalosantoss@gmail.com>
2023-09-13 20:33:25 -0300

    Get student information from prompt

--- apps/app.c
+++ apps/app.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "manager.h"
 
 int main(int argc, char *argv[])
 {
+    if(argc != 5) exit(-1);
+
+    int    age        = atoi(argv[2]);
+    double reg_number = atof(argv[3]);
+    float  cre        = atof(argv[4]);
+
     profile *student = MANAGER_createStudentProfile("Adam", 14, 20231200, 0.0);
 
     MANAGER_printStudentProfile(student);
