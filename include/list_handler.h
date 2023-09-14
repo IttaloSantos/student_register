2501dd2888ca24e051909597897017bc293a8f2a
IttaloSantos <ittalosantoss@gmail.com>
2023-09-13 21:49:52 -0300

    Add method to insert node in the end of the list

--- include/list_handler.h
+++ include/list_handler.h
@@ -8,6 +8,7 @@ typedef struct list_st linked_list;
 typedef struct node_st node;
 
 linked_list *LIST_createList(void);
-bool LIST_addNodeOnHead(linked_list *list, uint8_t *value_p, int value_size);
+bool LIST_addNodeOnHead(linked_list *list, const uint8_t *value_p, int value_size);
+bool LIST_addNodeOnTail(linked_list *list, const uint8_t *value_p, int value_size);
 
 #endif
--- src/list_handler.c
+++ src/list_handler.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include "list_handler.h"
 
-static bool is_list_empty(linked_list *list);
+static bool is_list_empty(const linked_list *list);
+static bool is_value_valid(const uint8_t *value_p, int value_size);
+static node *create_node(const uint8_t *value_p, int value_size);
 
 /* ######################################################### */
 /*                          PUBLIC                           */
@@ -27,18 +29,11 @@ linked_list *LIST_createList(void)
     return list;
 }
 
-bool LIST_addNodeOnHead(linked_list *list, uint8_t *value_p, int value_size)
+bool LIST_addNodeOnHead(linked_list *list, const uint8_t *value_p, int value_size)
 {
-    if(value_size == 0 || value_p == NULL)
-    {
-        printf("Wrong value.");
-        return false;
-    }
+    if(!is_value_valid(value_p, value_size)) return false;
 
-    node *node_p  = (node*)    calloc(1, sizeof(node));
-    node_p->value = (uint8_t*) calloc(1, value_size*sizeof(uint8_t));
-
-    memcpy(node_p->value, value_p, value_size);
+    node *node_p = create_node(value_p, value_size);
 
     if(is_list_empty(list)) list->end = node_p;
 
@@ -48,11 +43,46 @@ bool LIST_addNodeOnHead(linked_list *list, uint8_t *value_p, int value_size)
     return true;
 }
 
+bool LIST_addNodeOnTail(linked_list *list, const uint8_t *value_p, int value_size)
+{
+    if(!is_value_valid(value_p, value_size)) return false;
+
+    node *node_p = create_node(value_p, value_size);
+
+    if(is_list_empty(list)) list->begin     = node_p;
+    else                    list->end->next = node_p;
+
+    list->end = node_p;
+
+    return true;
+}
+
 /* ######################################################### */
 /*                          PRIVATE                          */
 /* ######################################################### */
 
-static bool is_list_empty(linked_list *list)
+static bool is_list_empty(const linked_list *list)
 {
     return list->begin == NULL && list->end == NULL;
+}
+
+static bool is_value_valid(const uint8_t *value_p, int value_size)
+{
+    if(value_size == 0 || value_p == NULL)
+    {
+        printf("Wrong value.");
+        return false;
+    }
+
+    return true;
+}
+
+static node *create_node(const uint8_t *value_p, int value_size)
+{
+    node *node_p  = (node*)    calloc(1, sizeof(node));
+    node_p->value = (uint8_t*) calloc(1, value_size*sizeof(uint8_t));
+
+    memcpy(node_p->value, value_p, value_size);
+
+    return node_p;
 }
