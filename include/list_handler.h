86e1140d7b082cf4d821ba4932ffcc704082fded
IttaloSantos <ittalosantoss@gmail.com>
2023-09-13 21:16:41 -0300

    Add method to insert a node to the list

--- include/list_handler.h
+++ include/list_handler.h
@@ -2,10 +2,12 @@
 #define LIST_HANDLER_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct list_st linked_list;
 typedef struct node_st node;
 
 linked_list *LIST_createList(void);
+bool LIST_addNodeOnHead(linked_list *list, uint8_t *value_p, int value_size);
 
 #endif
--- src/list_handler.c
+++ src/list_handler.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include "list_handler.h"
 
+static bool is_list_empty(linked_list *list);
+
+/* ######################################################### */
+/*                          PUBLIC                           */
+/* ######################################################### */
+
 struct node_st
 {
     uint8_t *value;
@@ -19,4 +25,34 @@ linked_list *LIST_createList(void)
 {
     linked_list *list = (linked_list*) calloc(1, sizeof(linked_list));
     return list;
+}
+
+bool LIST_addNodeOnHead(linked_list *list, uint8_t *value_p, int value_size)
+{
+    if(value_size == 0 || value_p == NULL)
+    {
+        printf("Wrong value.");
+        return false;
+    }
+
+    node *node_p  = (node*)    calloc(1, sizeof(node));
+    node_p->value = (uint8_t*) calloc(1, value_size*sizeof(uint8_t));
+
+    memcpy(node_p->value, value_p, value_size);
+
+    if(is_list_empty(list)) list->end = node_p;
+
+    node_p->next = list->begin;
+    list->begin  = node_p;
+
+    return true;
+}
+
+/* ######################################################### */
+/*                          PRIVATE                          */
+/* ######################################################### */
+
+static bool is_list_empty(linked_list *list)
+{
+    return list->begin == NULL && list->end == NULL;
 }
