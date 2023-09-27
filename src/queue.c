f33b7dcb7808434e9115324c0c1308b9a5339cf0
IttaloSantos <ittalosantoss@gmail.com>
2023-09-26 21:21:36 -0300

    Add method to create queue

--- src/queue.c
+++ src/queue.c
@@ -6,3 +6,11 @@ struct queue_st
 {
     list *data;
 };
+
+queue *QUEUE_createQueue(void)
+{
+    queue *queue_p = (queue*) calloc(1, sizeof(queue));
+    queue_p->data  = LIST_createList();
+
+    return queue_p;
+}
